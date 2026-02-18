d905f806d09be90db5e7c34672a55c2529d92471
Kojo Bailey <kojomolojo@gmail.com>
2026-02-18 15:30:58 +0000

    ":w

--- player-color-param/src/lib.rs
+++ player-color-param/src/lib.rs
@@ -1,1 +1,14 @@
 use player_color_param::PlayerColorParam;
+
+pub PlayerColorParamJson {
+}
+
+pub fn to_json(param: &PlayerColorParam) -> Result<String, Error> {
+    return "placeholder".to_string();
+}
+
+pub fn from_json(json: &str) -> Result<PlayerColorParam, Error> {
+    PlayerColorParam {
+        entries: std::collections::HashMap::new(),
+    }
+}
